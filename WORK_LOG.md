d0395fd903d2193dd8f987d1b2b0411bf12eaea8
sabrinavohra <sabrina.vohra@menloschool.org>
2025-03-09 17:01:28 -0700

    Brainstorm

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
-Name: YOUR NAME HERE
+Name: Sabrina Vohra
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
-
+| Date |      Time       |                                                                                                              Update |
+|:-----|:---------------:|--------------------------------------------------------------------------------------------------------------------:|
+| 3/5  |  2:35 - 3:00pm  |                                                                                   I started brainstorming in class. |
+| 3/6  | 12:55 - 1:45pm  |                                             I continued brainstorming in class and started writing down some ideas. |
+| 3/9  | 11:25 - 11:55am | I continued brainstorming about the structure of the program and process it should follow and wrote out some ideas. |
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Autocorrect.java
+++ src/Autocorrect.java
@@ -8,9 +8,27 @@ import java.io.IOException;
  * A command-line tool to suggest similar words when given one not in the dictionary.
  * </p>
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Sabrina Vohra
  */
 public class Autocorrect {
+    String[] dictionary;
+    int threshold;
+
+    // Main idea:
+        //Dictionary --> candidates --> (solve for edit distance) final list in order of edit distance
+    // Process:
+    // Set threshold
+    // Is good word?
+    // Have done problem before?
+    // Make smaller by:
+            // +- 2 in word length
+            // Do k-2 strings (k grams)
+    // Check all candidates
+        // Save edit distances in Array
+    // Organize words by edit distance
+        // Organize words by alphabetical order within edit distance value
+    // Print out sorted words
+
 
     /**
      * Constucts an instance of the Autocorrect class.
@@ -18,7 +36,8 @@ public class Autocorrect {
      * @param threshold The maximum number of edits a suggestion can have.
      */
     public Autocorrect(String[] words, int threshold) {
-
+        dictionary = words;
+        this.threshold = threshold;
     }
 
     /**
@@ -28,7 +47,6 @@ public class Autocorrect {
      * to threshold, sorted by edit distnace, then sorted alphabetically.
      */
     public String[] runTest(String typed) {
-
         return new String[0];
     }
 
